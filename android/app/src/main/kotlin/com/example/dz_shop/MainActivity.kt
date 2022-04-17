20be7b0d97a3f6d402a5f6b04ef2613f2a8d6f1f
rabah <rabahham01@gmail.com>
2022-04-17 13:16:51 +0200

    dz_shop

---
+++ android/app/src/main/kotlin/com/example/dz_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dz_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dz_shop/screens/on_bording/on_bording.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OnBordingScreen(),
+    );
+  }
+}
